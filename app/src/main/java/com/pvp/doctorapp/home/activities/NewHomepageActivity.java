b0089d8784e2341c8c216d4a64fc0f217a0947a2
Vishal Landepatil <vishallandepatil@gmail.comgradlew>
2020-08-29 18:18:01 +0530

    commite

--- app/src/main/java/com/pvp/doctorapp/home/activities/NewHomepageActivity.java
+++ app/src/main/java/com/pvp/doctorapp/home/activities/NewHomepageActivity.java
@@ -1,47 +1,65 @@
 package com.pvp.doctorapp.home.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 import com.pvp.doctorapp.databinding.ActivityNewhomepageBinding;
+import com.pvp.doctorapp.doctore.fragments.DoctoreFragment;
 import com.pvp.doctorapp.home.fragments.HomeFragment;
 import com.pvp.doctorapp.R;
-import com.pvp.doctorapp.activities.navigation.CustomBottomNavigationView1;
+import com.pvp.doctorapp.hospital.fragments.HospitalFragment;
+
+import java.util.List;
 
 
 public class NewHomepageActivity extends AppCompatActivity
         {
             Fragment fragment;
 
+            ActivityNewhomepageBinding binding;
             @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
 
-             ActivityNewhomepageBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_newhomepage);
+              binding = DataBindingUtil.setContentView(this, R.layout.activity_newhomepage);
              binding.customBottomBar.inflateMenu(R.menu.bottom_menu);
-             loadFragment(new HomeFragment());
+             binding.customBottomBar.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_UNLABELED);
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_container, new HomeFragment()).commit();;
 
                 binding.customBottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @SuppressLint("RestrictedApi")
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
                         switch (menuItem.getItemId()){
 
                             case R.id.navigation_home:
-
-                                Toast.makeText(getBaseContext(),"Home",Toast.LENGTH_LONG).show();
+                                transaction.replace(R.id.frame_container, new HomeFragment()).addToBackStack(null).commit();;
+                                binding.customBottomBar.setVisibility(View.VISIBLE);
+                                binding.fab.setVisibility(View.VISIBLE);
                                 break;
-                            case R.id.navigation_bl:
-
-                                Toast.makeText(getBaseContext(),"2nd",Toast.LENGTH_LONG).show();
+                            case R.id.navigation_doctor:
+                                transaction.replace(R.id.frame_container, new DoctoreFragment()).addToBackStack(null).commit();;
+                                binding.customBottomBar.setVisibility(View.GONE);
+                                binding.fab.setVisibility(View.GONE);
+                            break;
+                            case R.id.navigation_hospital:
+                                transaction.replace(R.id.frame_container, new HospitalFragment()).addToBackStack(null).commit();;
+                                binding.customBottomBar.setVisibility(View.GONE);
+                                binding.fab.setVisibility(View.GONE);
                             break;
 
 
@@ -57,15 +75,29 @@ public class NewHomepageActivity extends AppCompatActivity
 
 
 
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onBackPressed() {
+                super.onBackPressed();
+                    if (getCurrentFragment() instanceof HomeFragment) {
+
+                        binding.customBottomBar.setVisibility(View.VISIBLE);
+                        binding.fab.setVisibility(View.VISIBLE);
+                        binding.customBottomBar.setSelectedItemId(R.id.navigation_home);
+                    }
 
 
-            private void loadFragment(Fragment fragment) {
-                // load fragment
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.frame_container, fragment);
-                transaction.commit();
+
             }
 
+
+            private Fragment getCurrentFragment(){
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                List<Fragment> list = fragmentManager.getFragments();
+                //get last one
+                Fragment currentFragment = list.get(list.size() - 1);
+                return currentFragment;
+            }
 
 
 
